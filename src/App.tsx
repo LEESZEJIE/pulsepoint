17f5ad67028ffbfe8f6848c919cbacced3fc85c6
jim-low <robil12380@gmail.com>
2024-11-17 17:34:22 +0800

    feat: redirect user to login page by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  if (location.pathname === '/') {
+    navigate('/login');
+    return;
+  }
+
   return (
     <div id='app'>
       <div className='bg'></div>
