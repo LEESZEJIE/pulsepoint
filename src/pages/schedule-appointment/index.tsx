243a7bd957d285da45be1287fac0af9341fce110
jim-low <robil12380@gmail.com>
2024-11-17 17:40:22 +0800

    fix: adjust calendar width and view

--- src/pages/schedule-appointment/index.tsx
+++ src/pages/schedule-appointment/index.tsx
@@ -7,26 +7,27 @@ import { Calendar, CalendarProps } from "antd";
 import { Dayjs } from "dayjs";
 
 const ScheduleAppointmentPage = () => {
-    const navigate = useNavigate();
-    const [selectedDoctor] = useRecoilState(selectedDoctorState);
-    
-    if (selectedDoctor.name === '') {
-        navigate('/home');
-        return;
-    }
-    
-    function handlePanelChange(value: Dayjs, mode: CalendarProps<Dayjs>['mode']) {
-        console.log(value.format('YYYY-MM-DD'), mode);
-    }
+  const navigate = useNavigate();
+  const [selectedDoctor] = useRecoilState(selectedDoctorState);
 
-    return (
-        <Flex id='schedule-appointment-page' className="page" justify='center' align='center'>
-            <DoctorCard doctor={selectedDoctor} />
-            <Box>
-                <Calendar onPanelChange={handlePanelChange} />
-            </Box>
-        </Flex>
-    )
+  if (selectedDoctor.name === '') {
+    navigate('/home');
+    return;
+  }
+
+  function handlePanelChange(value: Dayjs, mode: CalendarProps<Dayjs>['mode']) {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  }
+
+  return (
+    <Flex id='schedule-appointment-page' className="page" justify='center' align='center'>
+      <DoctorCard doctor={selectedDoctor} />
+      <Box width="40%" style={{ border: '1px solid black', borderRadius: 8 }} p='3'>
+        <Calendar fullscreen={false} onPanelChange={handlePanelChange} />
+      </Box>
+
+    </Flex>
+  )
 }
 
 export default ScheduleAppointmentPage
