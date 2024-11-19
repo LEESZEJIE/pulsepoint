7bd5f39ee2f1153ba2ab44bd64db94e10c34def8
jim-low <robil12380@gmail.com>
2024-11-20 00:33:08 +0800

    feat: add excluded paths to prevent redundant function call

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 import { useRecoilState } from 'recoil';
 import { loggedInUserState } from './state';
 import { useEffect } from 'react';
 
+const excludedPaths = ['login', 'register'];
+
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
   const [loggedInUser] = useRecoilState(loggedInUserState);
 
   useEffect(() => {
-    if (loggedInUser == null) {
+    if (loggedInUser == null && !excludedPaths.includes(location.pathname.substring(1))) {
       navigate('/login');
     }
   }, [])
