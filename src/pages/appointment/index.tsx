4a808fd50a276ac6ce6ba6b14013e880b15b4feb
jim-low <robil12380@gmail.com>
2024-11-21 22:58:50 +0800

    feat: show empty indicator when appointments list is empty

--- src/pages/appointment/index.tsx
+++ src/pages/appointment/index.tsx
@@ -1,8 +1,10 @@
 import { Box, Flex } from "@radix-ui/themes";
-import { Tabs } from "antd";
+import { Empty, Tabs } from "antd";
 import './index.css';
 import UpcomingAppointments from "./components/upcoming-appointments";
 import FinishedAppointments from "./components/finished-appointments";
+import { useRecoilState } from "recoil";
+import { appointmentsListState } from "../../state";
 
 const tabsItems = [
   {
@@ -18,10 +20,13 @@ const tabsItems = [
 ]
 
 const AppointmentsPage = () => {
+  const [appointmentsList] = useRecoilState(appointmentsListState);
+
   return (
     <Box id='appointments-page' className='page'>
       <Flex direction='column' className="appointments-container" p='3'>
-        <Tabs defaultActiveKey="upcoming" items={tabsItems} />
+          <Tabs defaultActiveKey="upcoming" items={tabsItems} />
+        { appointmentsList.length === 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} /> }
       </Flex>
     </Box>
   )
