1c8a8cd1299cadea112e5e97bbb67624fa681614
jim-low <robil12380@gmail.com>
2024-12-09 20:33:07 +0800

    feat: add edit profile function

--- src/pages/user-details/index.tsx
+++ src/pages/user-details/index.tsx
@@ -3,34 +3,71 @@ import { useRecoilState } from "recoil"
 import { loggedInUserState } from "../../state";
 import { useNavigate } from "react-router-dom";
 import './index.css'
-import { Button, message } from "antd";
-import React, { useEffect } from "react";
+import { Button, Input, message, Modal } from "antd";
+import { useState } from "react";
 
 const UserDetailsPage = () => {
-  const [messageApi, contextHolder] = message.useMessage();
-
   const navigate = useNavigate();
   const [user, setUser] = useRecoilState(loggedInUserState);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editFields, setEditFields] = useState({
+    fullname: "",
+    contact: "",
+    address: "",
+    password: "",
+    retypepassword: "",
+  });
+  const [messageApi, contextHolder] = message.useMessage();
+
+  function handleEditFieldOnChange(category: 'fullname' | 'contact' | 'address' | 'password' | 'retypepassword', value: string) {
+    setEditFields(prev => {
+      return {
+        ...prev,
+        [category]: value
+      };
+    })
+  }
 
   function handleLogout() {
     setUser(null);
     navigate('/login')
   }
 
-  useEffect(() => {
-    if (user != null) {
+  function handleReadyToEdit() {
+    setIsEditModalOpen(true);
+
+    const { fullname = '', contact = '', address = '' } = user ?? {};
+    setEditFields({
+      fullname,
+      contact,
+      address,
+      password: '',
+      retypepassword: '',
+    });
+  }
+
+  function handleEdit() {
+    if (editFields.password !== editFields.retypepassword) {
+      messageApi.error('Passwords do not match!');
       return;
     }
 
-    messageApi.info('Nothing to view in Guest Mode');
-  }, [])
+    const { fullname, contact, address, password } = editFields;
+    setUser(prev => {
+      return {
+        ...prev,
+        fullname,
+        contact,
+        address,
+        password,
+      };
+    })
+    setIsEditModalOpen(false);
+    messageApi.success('Account information successfully updated!');
+  }
 
   if (user == null) {
-    return (
-      <React.Fragment>
-        {contextHolder}
-      </React.Fragment>
-    );
+    return null;
   }
 
   const { fullname, nric, contact, address } = user ?? {};
@@ -65,10 +102,51 @@ const UserDetailsPage = () => {
         <Button onClick={() => navigate(-1)} type='primary'>
           Back
         </Button>
+        <Button onClick={handleReadyToEdit} type='primary'>
+          Edit
+        </Button>
         <Button onClick={handleLogout} danger>
           Logout
         </Button>
       </Flex>
+
+      <Modal
+        open={isEditModalOpen}
+        okText="Confirm"
+        onOk={handleEdit}
+        onClose={() => setIsEditModalOpen(false)}
+        closable={true}
+        centered
+        onCancel={() => setIsEditModalOpen(false)}
+        title="Edit Profile Info"
+      >
+        <Flex direction='column'>
+          <Text as="p" m="0">Full Name</Text>
+          <Input style={{ marginBottom: '15px' }} placeholder="Full Name" value={editFields.fullname} onChange={e => handleEditFieldOnChange('fullname', e.target.value)} />
+        </Flex>
+
+        <Flex direction='column'>
+          <Text as="p" m="0">Contact Number</Text>
+          <Input style={{ marginBottom: '15px' }} placeholder="Contact Number" value={editFields.contact} onChange={e => handleEditFieldOnChange('contact', e.target.value)} />
+        </Flex>
+
+        <Flex direction='column'>
+          <Text as="p" m="0">Address</Text>
+          <Input style={{ marginBottom: '15px' }} placeholder="Address" value={editFields.address} onChange={e => handleEditFieldOnChange('address', e.target.value)} />
+        </Flex>
+
+        <Flex direction='column'>
+          <Text as="p" m="0">Password</Text>
+          <Input style={{ marginBottom: '15px' }} placeholder="Password" type="password" value={editFields.password} onChange={e => handleEditFieldOnChange('password', e.target.value)} />
+        </Flex>
+
+        <Flex direction='column'>
+          <Text as="p" m="0">Retype Password</Text>
+          <Input style={{ marginBottom: '15px' }} placeholder="Retype Password" type="password" value={editFields.retypepassword} onChange={e => handleEditFieldOnChange('retypepassword', e.target.value)} />
+        </Flex>
+      </Modal>
+
+      {contextHolder}
     </Flex>
   )
 }
